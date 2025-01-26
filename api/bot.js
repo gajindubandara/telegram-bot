ef7e3791a4d045e2d104b0f409df5018aab2de6f
gajindubandara <gajindukb@gmail.com>
2025-01-26 15:09:29 +0530

    Comment unused code

--- api/bot.js
+++ api/bot.js
@@ -1,35 +1,35 @@
-import TelegramBot from 'node-telegram-bot-api';
-
-const token = process.env.TELEGRAM_BOT_TOKEN;
-const chatId = process.env.TELEGRAM_GROUP_CHAT_ID;
-
-// Log environment variables to check if they are loaded correctly
-console.log('Environment Variables:', { token, chatId });
-
-if (!token || !chatId) {
-  console.error('Error: Missing environment variables. Ensure TELEGRAM_BOT_TOKEN and TELEGRAM_GROUP_CHAT_ID are set.');
-  process.exit(1);
-}
-
-// Initialize the bot
-const bot = new TelegramBot(token, { polling: true });
-
-// Function to send the timestamp message
-const sendTimestampMessage = () => {
-  const timestamp = new Date().toLocaleString(); // Get the current timestamp
-  const message = `Current Timestamp: ${timestamp}`;
-  console.log('Sending message:', message); // Log the message being sent
-
-  bot.sendMessage(chatId, message)
-      .then(() => console.log('Message sent successfully!'))
-      .catch((error) => console.error('Error sending message:', error));
-};
-
-sendTimestampMessage();
-// Set up an interval to send a timestamp message every minute
-// setInterval(sendTimestampMessage, 60000);
-
-console.log('Bot is running and will send a timestamp every minute...');
+// import TelegramBot from 'node-telegram-bot-api';
+//
+// const token = process.env.TELEGRAM_BOT_TOKEN;
+// const chatId = process.env.TELEGRAM_GROUP_CHAT_ID;
+//
+// // Log environment variables to check if they are loaded correctly
+// console.log('Environment Variables:', { token, chatId });
+//
+// if (!token || !chatId) {
+//   console.error('Error: Missing environment variables. Ensure TELEGRAM_BOT_TOKEN and TELEGRAM_GROUP_CHAT_ID are set.');
+//   process.exit(1);
+// }
+//
+// // Initialize the bot
+// const bot = new TelegramBot(token, { polling: true });
+//
+// // Function to send the timestamp message
+// const sendTimestampMessage = () => {
+//   const timestamp = new Date().toLocaleString(); // Get the current timestamp
+//   const message = `Current Timestamp: ${timestamp}`;
+//   console.log('Sending message:', message); // Log the message being sent
+//
+//   bot.sendMessage(chatId, message)
+//       .then(() => console.log('Message sent successfully!'))
+//       .catch((error) => console.error('Error sending message:', error));
+// };
+//
+// sendTimestampMessage();
+// // Set up an interval to send a timestamp message every minute
+// // setInterval(sendTimestampMessage, 60000);
+//
+// console.log('Bot is running and will send a timestamp every minute...');
 
 
 
